0c3ff077ddbc1304ec952f4e99056e24a05381d8
Piotr Walat <pwalat98@gmail.com>
2020-11-26 22:03:24 +0100

    init

--- client/__main__.py
+++ client/__main__.py
@@ -71,3 +71,29 @@ if __name__ == "__main__":
     # main(example1=example1)
 
 main()
+
+# main function to start adding solution
+def add_solution_to_tree(root:SpaCyTreeNode):
+    print("Sorry I can't help you, Can you tell me how to solve it?")
+    user_solution = input("*your solution** >")
+    # TODO generate doc
+    best_branch_node = choose_best_branch(root, user_solution, 0)
+    choosen_node = search_good_node_to_add_solution_to_in_branch(best_branch_node, 0.8, user_solution)
+    ignore = 1
+    while choosen_node is None:
+        best_branch_node = choose_best_branch(root, user_solution, ignore)
+        choosen_node = search_good_node_to_add_solution_to_in_branch(best_branch_node, 0.8, user_solution)
+        ignore += ignore
+    else:
+        choosen_node.add_solution(user_solution)
+
+
+
+
+# function which choose the best branch from root to start searching through
+def choose_best_branch(root:SpaCyTreeNode, user_solution, how_many_first_ignore):
+    return True
+
+# function which returns best node match to gotten solution or null if in branch there is none matching treshold
+def search_good_node_to_add_solution_to_in_branch(branch_node:SpaCyTreeNode, treshold:int, user_solution):
+    return True
