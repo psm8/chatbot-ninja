6e57a94aec8e75e6e55ae64151c3a6f5a03e22e7
psm8 <pioseem8@gmail.com>
2020-11-26 23:18:25 +0100

    model tests

--- client/__main__.py
+++ client/__main__.py
@@ -1,25 +1,27 @@
 import argparse
 
 from preprocess.preprocess import Preprocess
-from dqn.conversation_manager import DQNAgent
+from t5.model import get_model
 
 
 def main():
 
-    preprocess = Preprocess()
-    dqn_agent = DQNAgent()
+    # preprocess = Preprocess()
+    model = get_model()
 
     while True:
-        preprocessed_input = preprocess.preprocess(input(">"))
-        for token in preprocessed_input:
-            print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-                  token.shape_, token.is_alpha, token.is_stop)
-        for ent in preprocessed_input.ents:
-            print(ent.text, ent.start_char, ent.end_char, ent.label_)
-        for word in preprocessed_input:
-            lexeme = preprocessed_input.vocab[word.text]
-            print(lexeme.text, lexeme.orth, lexeme.shape_, lexeme.prefix_, lexeme.suffix_,
-                  lexeme.is_alpha, lexeme.is_digit, lexeme.is_title, lexeme.lang_)
+        model.predict("I remove the engine!	ask_question")
+        inp = input(">")
+        # preprocessed_input = preprocess.preprocess(inp)
+        # for token in preprocessed_input:
+        #     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+        #           token.shape_, token.is_alpha, token.is_stop)
+        # for ent in preprocessed_input.ents:
+        #     print(ent.text, ent.start_char, ent.end_char, ent.label_)
+        # for word in preprocessed_input:
+        #     lexeme = preprocessed_input.vocab[word.text]
+        #     print(lexeme.text, lexeme.orth, lexeme.shape_, lexeme.prefix_, lexeme.suffix_,
+        #           lexeme.is_alpha, lexeme.is_digit, lexeme.is_title, lexeme.lang_)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Chatbot Ninja")
