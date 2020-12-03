e1c810c957d8fd0887bc3f785de589cc7bbace80
Liptonski <mcieslik@student.agh.edu.pl>
2020-12-03 20:04:20 +0100

    using large en core web

--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -1,6 +1,6 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_lg")
 # tagger parser and ner added by default
 # self.nlp.add_pipe(self.nlp.create_pipe("entity_linker"))
 # self.nlp.add_pipe(self.nlp.create_pipe("textcat"))
