83fdce14d73e72f306340cf97274e85607086d1d
psm8 <pioseem8@gmail.com>
2020-12-11 16:29:50 +0100

    changes preprop

--- t5/preprop.py
+++ t5/preprop.py
@@ -1,12 +1,10 @@
 import fnmatch
 import os
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
-from multiprocessing import Process, Queue
 
 
-RIGHT_QUESTIONS = ["did you tr", "have you tr", "did you considered ", "could you ", "can you ", "you might "]
+RIGHT_QUESTIONS = ["did you tr", "have you tr", "did you considered ", "you might "]
 
 
 def parse(file):
@@ -28,7 +26,7 @@ def getDF(path):
 def choose_right_questions(path):
     data = {'target_text':[], 'input_text':[], 'prefix':[]}
     for file in os.listdir(path):
-        if fnmatch.fnmatch(file, 'dialogueText*.csv'):
+        if fnmatch.fnmatch(file, 'dialogueText.csv'):
             print(file)
             df = pd.read_csv(path + "/" +file)
             df2 = df.drop(columns=['folder', 'date'])
@@ -44,8 +42,8 @@ def choose_right_questions(path):
 
     train_df, eval_df = train_test_split(result_df, test_size=0.05)
 
-    train_df.to_csv("data/ninja_train_df.tsv", "\t")
-    eval_df.to_csv("data/ninja_eval_df.tsv", "\t")
+    train_df.to_csv("data/ninja_train_df2.tsv", "\t")
+    eval_df.to_csv("data/ninja_eval_df2.tsv", "\t")
 
 
 def is_right_question(text):
@@ -58,16 +56,21 @@ def is_right_question(text):
 
 def append_result(data, group, id1):
     local_text = ''
-    for id2, text2 in group.iterrows():
-        if id1 - 5 <= id2:
-            if id2 < id1:
-                if isinstance(text2['text'], str):
-                    local_text += (text2['text'] + " ")
-            else:
-                break
+    max_id = max([id_test for id_test, _ in group.iterrows()])
+    if max_id > id1:
+        for id2, text2 in group.iterrows():
+            if id1 - 5 <= id2:
+                if id2 < id1:
+                    if isinstance(text2['text'], str):
+                        local_text += (text2['text'] + " ")
+                elif id2 == id1:
+                    target_text = (text2['text'] + " ")
+                else:
+                    break
 
     if local_text != "":
-        data['target_text'].append(text2['text'])
+        data['target_text'].append(target_text)
+        local_text += text2['text']
         data['input_text'].append(local_text)
         data['prefix'].append('CHAT')
 
